ee0a0ccbed25ceef1b57df42adfc1512061fd501
Bohdan Stasiv <badyaxa@gmail.com>
2025-01-11 03:02:11 +0200

    first draft version

---
+++ src/main/java/com/leonbets/model/EventData.java
@@ -0,0 +1,18 @@
+package com.leonbets.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class EventData {
+    private List<Event> events;
+}
---
+++ src/main/java/com/leonbets/model/SportsEvent.java
@@ -0,0 +1,19 @@
+package com.leonbets.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SportsEvent {
+    private String name;
+    private List<Region> regions;
+}
